00ab5ea2de12b4cb0f4ad749893da53b978bdc99
RobertDowney-Jr <1243134432@qq.com>
2024-05-03 18:42:02 +0800

    init

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
 
 
 
+
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
